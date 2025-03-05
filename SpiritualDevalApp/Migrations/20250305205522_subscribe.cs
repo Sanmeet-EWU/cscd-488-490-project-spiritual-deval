1c6f2def36a1e363722116e071dabc84ed88dae8
Aramirez61 <aramirez61@ewu.edu>
2025-03-05 13:03:33 -0800

    Add subscription button and mail sending

---
+++ SpiritualDevalApp/Migrations/20250305205522_subscribe.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpiritualDevalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class subscribe : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "subscribed_to_volunteer_updates",
+                table: "UserAccounts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "subscribed_to_volunteer_updates",
+                table: "UserAccounts");
+        }
+    }
+}
